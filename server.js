58fe568f4694a47d3a5ddacc0eff75e1fb247f6c
Matt Porter <matthew.ellis.porter@gmail.com>
2022-09-01 12:34:08 -0400

    project set up

---
+++ server.js
@@ -0,0 +1,31 @@
+//////// DEPENDENCIES
+
+const express = require("express");
+const app = express();
+const port= 3000;
+
+////// DATABASE
+
+const pokedex = require("./models/pokemon");
+
+
+// I
+
+// N
+
+// D
+
+// U
+
+// C
+
+// E
+
+// S
+
+
+/// app listener
+
+app.listen(port, function(){
+    console.log(`Gotta catch 'em all on ${port}`)
+})
